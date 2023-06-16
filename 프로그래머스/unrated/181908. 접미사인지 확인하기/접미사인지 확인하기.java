5fdda88ed888e8efb56e2a78bf53bbf318b262bd
tiny-stone <131943335+kwchoi11@users.noreply.github.com>
2023-06-16 19:31:19 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String my_string, String is_suffix) {
         int answer = 0;
         
-        // is_suffix의 길이가 my_string보다 길거나 같은 경우에만 접미사가 될 수 있음. 
+        // is_suffix의 길이가 my_string보다 짧거나 같은 경우에만 접미사가 될 수 있음. 
         if (is_suffix.length() <= my_string.length()) {
             int suffixLength = is_suffix.length();
             String suffix = my_string.substring(my_string.length() - suffixLength);
