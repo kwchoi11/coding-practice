4dc7c70f27fe4e1b332fdb61992b026b41a7dec4
tiny-stone <131943335+kwchoi11@users.noreply.github.com>
2023-06-23 00:26:30 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 5.94 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 50 && arr[i] % 2 == 0) {
                 answer[i] = arr[i] / 2;
-            } else if (arr[i] < 50 && arr[i] % 2 == 1) {
+            } else if (arr[i] < 50 && arr[i] % 2 != 0) {
                 answer[i] = arr[i] * 2;
             } else {
                 answer[i] = arr[i];
