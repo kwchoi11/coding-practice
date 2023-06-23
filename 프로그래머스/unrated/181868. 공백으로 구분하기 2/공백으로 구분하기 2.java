ab16fddd2dc8135918c12e06894e6e2ed27b5723
tiny-stone <131943335+kwchoi11@users.noreply.github.com>
2023-06-23 19:05:13 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.43 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] words = my_string.trim().split("\\s+");
+        String[] answer = new String[words.length];
+        
+        for (int i = 0; i < words.length; i++) {
+            answer[i] = words[i];
+        }
+        return answer;
+    }
+}
